ce3ee0b501ab84cc4943404d68779746c4a80ace
dylan98ed <dylanquiroga98@gmail.com>
2025-03-08 17:00:12 -0300

    wip imu.cpp

--- source/periodics/imu.cpp
+++ source/periodics/imu.cpp
@@ -31,14 +31,13 @@
 
 #include <periodics/imu.hpp>
 #include <cmath>  // Para sin(), cos(), M_PI
+#ifndef M_PI
+    #define M_PI 3.1415926535897932
+#endif
 
-#define M_PI 3.1415926535897932
-
-#define _100_chars                      100
-#define BNO055_EULER_DIV_DEG_int        16
-#define BNO055_LINEAR_ACCEL_DIV_MSQ_int 100
-#define precision_scaling_factor        1000
-
+#define _100_chars                      100     // tamaño del buffer
+#define BNO055_EULER_DIV_DEG_int        16.0    // division para envio de variables por monitor serial
+#define BNO055_LINEAR_ACCEL_DIV_MSQ_int 100     // variable para conversion de datos
 #define PERIOD 10 // old:MAX_NOISE
 
 namespace periodics{
@@ -49,12 +48,12 @@ namespace periodics{
             UnbufferedSerial& f_serial,
             PinName SDA,
             PinName SCL)
-        : utils::CTask(f_period)
-        , m_isActive(false)
-        , m_serial(f_serial)
+        : utils::CTask(f_period),
+        m_isActive(false),
+        m_serial(f_serial),
+        dt(0.01)
     {
-        dt = 0.01;
-
+        
         A_ << 1, dt, 0, 0,
               0, 1, 0, 0,
               0, 0, 1, dt,
@@ -83,18 +82,20 @@ namespace periodics{
         x_(3) = 0.0; // Velocidad en Y
 
         m_messageSendCounter = 0;
-
+        //Se configura el período de ejecución del hilo
         if(m_delta_time < PERIOD){
             setNewPeriod(PERIOD);
             m_delta_time = PERIOD;
         }
-        
+        /*
+        *Se inicializa el sensor IMU (BNO055) a través de I2C. Se configura el *modo de operación, el rango de aceleración y la unidad de medida para *los ángulos de Euler.
+        */
         s32 comres = BNO055_ERROR;
         u8 power_mode = BNO055_INIT_VALUE;
 
         printf("Starting IMU sensor data acquisition...\r\n");  
         i2c_instance = new I2C(SDA, SCL);
-        i2c_instance->frequency(400000);
+        i2c_instance->frequency(400000);    //frec maxima de i2c para el imu
 
         ThisThread::sleep_for(chrono::milliseconds(300));
 
@@ -115,16 +116,14 @@ namespace periodics{
         comres = bno055_get_euler_unit(&euler_unit_u8);
 
         if (euler_unit_u8 != BNO055_EULER_UNIT_DEG)
-        {
             comres += bno055_set_euler_unit(BNO055_EULER_UNIT_DEG);
-        }
 
     }
 
     CImu::~CImu()
     {
         /*-----------------------------------------------------------------------*
-        ************************* START DE-INITIALIZATION ***********************
+        ************************* START DE-INITIALIZATION **********************
         *-------------------------------------------------------------------------*/
         s32 comres = BNO055_ERROR;
         /* variable used to set the power mode of the sensor*/
@@ -149,7 +148,9 @@ namespace periodics{
         ************************* END DE-INITIALIZATION **********************
         *---------------------------------------------------------------------*/
     };
-
+    /*
+    *Esta función procesa comandos recibidos a través de la comunicación *serial para activar o desactivar el IMU.
+    */
     void CImu::serialCallbackIMUcommand(char const * a, char * b) {
         uint8_t l_isActivate=0;
         uint8_t l_res = sscanf(a,"%hhu",&l_isActivate);
@@ -169,7 +170,7 @@ namespace periodics{
             sprintf(b,"syntax error");
         }
     }
-
+    //comunicación I2C con el sensor IMU para escribir
     s8 CImu::BNO055_I2C_bus_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
     {
         s32 BNO055_iERROR = BNO055_INIT_VALUE;
@@ -183,59 +184,22 @@ namespace periodics{
         }
 
         if (i2c_instance->write(dev_addr, (const char*)array, cnt + 1) == 0)
-        {
-            BNO055_iERROR = BNO055_SUCCESS; // Return success (0)
-        }
-        else
-        {
-            BNO055_iERROR = BNO055_ERROR; // Return error (-1)
-        }
-
-        return (s8)BNO055_iERROR;
+            return BNO055_SUCCESS; // Return success (0)
+        return BNO055_ERROR;
     }
-
+    //comunicación I2C con el sensor IMU para leer
     s8 CImu::BNO055_I2C_bus_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
     {
-        s32 BNO055_iERROR = BNO055_INIT_VALUE;
-        u8 array[I2C_BUFFER_LEN] = { BNO055_INIT_VALUE };
-        u8 stringpos = BNO055_INIT_VALUE;
-
-        array[BNO055_INIT_VALUE] = reg_addr;
-
-        /* Please take the below API as your reference
-        * for read the data using I2C communication
-        * add your I2C read API here.
-        * "BNO055_iERROR = I2C_WRITE_READ_STRING(DEV_ADDR,
-        * ARRAY, ARRAY, 1, CNT)"
-        * BNO055_iERROR is an return value of SPI write API
-        * Please select your valid return value
-        * In the driver BNO055_SUCCESS defined as 0
-        * and FAILURE defined as -1
-        */
-        for (stringpos = BNO055_INIT_VALUE; stringpos < cnt; stringpos++)
-        {
-            *(reg_data + stringpos) = array[stringpos];
-        }
-
         // Write the register address to set the pointer for reading
         if (i2c_instance->write(dev_addr, (const char*)&reg_addr, 1) != 0)
-        {
-            BNO055_iERROR = BNO055_ERROR; // Return error (-1)
-            return (s8)BNO055_iERROR;
-        }
+            return BNO055_ERROR;
 
         // Read the data from the specified register address
         if (i2c_instance->read(dev_addr, (char*)reg_data, cnt) == 0)
-        {
-            BNO055_iERROR = BNO055_SUCCESS; // Return success (0)
-        }
-        else
-        {
-            BNO055_iERROR = BNO055_ERROR; // Return error (-1)
-        }
-        return (s8)BNO055_iERROR;
+            return BNO055_SUCCESS;
+        return BNO055_ERROR;
     }
-    
+    /*funcion de comunicacion*/
     void CImu::I2C_routine(void)
     {
         bno055.bus_write = BNO055_I2C_bus_write;
@@ -246,31 +210,34 @@ namespace periodics{
 
         ThisThread::sleep_for(chrono::milliseconds(300));
     }
-
+    /*funcion de comunicacion*/
     void CImu::BNO055_delay_msek(u32 msek)
     {
         /*Here you can write your own delay routine*/
         ThisThread::sleep_for(chrono::milliseconds(msek));
     }
-
+    /*filtro de kalman*/
     void CImu::predict(const Eigen::Vector2d& acceleration) {
         x_ = A_ * x_ + B_ * acceleration;
         P_ = A_ * P_ * A_.transpose() + Q_;
     }
 
+    /*filtro de kalman*/
     void CImu::update(const Eigen::Vector2d& position) {
         Eigen::Vector2d z = position;
         Eigen::Vector2d y = z - H_ * x_;
         Eigen::MatrixXd S = H_ * P_ * H_.transpose() + R_;
         Eigen::MatrixXd K = P_ * H_.transpose() * S.inverse();
-        x_ = x_ + K * y;
+        x_ += K * y;
         P_ = (Eigen::MatrixXd::Identity(4, 4) - K * H_) * P_;
     }
-
+    /*
+    *Esta función es el núcleo del hilo que se ejecuta periódicamente. Lee los *datos del IMU, aplica el filtro de Kalman y envía los resultados a través *de la comunicación serial.
+    */
     void CImu::_run() {
         /* Run method behaviour */
         if(!m_isActive) return;
-
+        
         char buffer[_100_chars];
         s8 comres = BNO055_SUCCESS;
 
@@ -280,13 +247,9 @@ namespace periodics{
         s16 s16_euler_h_raw = BNO055_INIT_VALUE;
         s16 s16_euler_p_raw = BNO055_INIT_VALUE;
         s16 s16_euler_r_raw = BNO055_INIT_VALUE;
-
+        /*se comunica con el thread bno055 que se comunica con el imu por i2c y obtine el valor de angulos*/
         comres += bno055_read_euler_h(&s16_euler_h_raw);
-        if(comres != BNO055_SUCCESS) return;
-
         comres += bno055_read_euler_p(&s16_euler_p_raw);
-        if(comres != BNO055_SUCCESS) return;
-
         comres += bno055_read_euler_r(&s16_euler_r_raw);
         if(comres != BNO055_SUCCESS) return;
 
@@ -295,16 +258,13 @@ namespace periodics{
         s16 s16_linear_accel_x_raw = BNO055_INIT_VALUE;
         s16 s16_linear_accel_y_raw = BNO055_INIT_VALUE;
         s16 s16_linear_accel_z_raw = BNO055_INIT_VALUE;
-
+        /*se comunica con el thread bno055 que se comunica con el imu por i2c y obtine el valor de aceleracion*/
         comres = bno055_read_linear_accel_x(&s16_linear_accel_x_raw);
-        if(comres != BNO055_SUCCESS) return;
-        
         comres = bno055_read_linear_accel_y(&s16_linear_accel_y_raw);
-        if(comres != BNO055_SUCCESS) return;
-
         comres = bno055_read_linear_accel_z(&s16_linear_accel_z_raw);
         if(comres != BNO055_SUCCESS) return;
         
+        /*converite detos que estan en raw mm/s² y se converten a m/s²*/
         double accelx = ((double)s16_linear_accel_x_raw) / BNO055_LINEAR_ACCEL_DIV_MSQ_int;
         double accely = ((double)s16_linear_accel_y_raw) / BNO055_LINEAR_ACCEL_DIV_MSQ_int;
         double accelz = ((double)s16_linear_accel_z_raw) / BNO055_LINEAR_ACCEL_DIV_MSQ_int;
@@ -317,13 +277,12 @@ namespace periodics{
         
         if (m_messageSendCounter >= 10)
         {
-
             m_messageSendCounter = 0;
             snprintf(buffer, sizeof(buffer), "@imu:%.3f;%.3f;%.3f;%.3f;%.3f;%.3f;%.3f;%.3f;%.3f;%.3f;%.3f;%.3f;;\r\n",  
             
-                (float)(s16_euler_h_raw/16.0), 
-                (float)(s16_euler_p_raw/16.0), 
-                (float)(s16_euler_r_raw/16.0), 
+                (float)(s16_euler_h_raw/BNO055_EULER_DIV_DEG_int), 
+                (float)(s16_euler_p_raw/BNO055_EULER_DIV_DEG_int), 
+                (float)(s16_euler_r_raw/BNO055_EULER_DIV_DEG_int), 
 
                 accelx,
                 accely,
